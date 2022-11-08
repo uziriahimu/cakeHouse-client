fcd5924f7e2942f2c465ff3ccc21582faff3b560
uziriahimu <uziriahimu@gmail.com>
2022-11-08 23:24:18 +0600

    first commit

---
+++ src/pages/Bannar/Bannar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Bannar = () => {
+    return (
+        <div >
+
+            <div>
+                <img className='w-full h-76' src={'https://marketplace.canva.com/EAE4zItWsNE/1/0/1600w/canva-pink-minimalist-cake-shop-on-facebook-post-spbmkxiOzP8.jpg'} alt="" /></div>
+        </div>
+    );
+};
+
+export default Bannar;
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Bannar from '../Bannar/Bannar';
+
+const Home = () => {
+    return (
+        <div>
+            <Bannar></Bannar>
+            <h2>Home</h2>
+        </div>
+    );
+};
+
+export default Home;
